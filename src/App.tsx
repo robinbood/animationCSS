c432565f43547ffeeb06c90a914ae68e13a6019b
robinbood <mercedesfarts@gmail.com>
2025-05-05 12:19:19 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import './App.css'
+
+const App = () => {
+ 
+  return (
+    <>
+      <div className='shit'>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        
+      
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </div>
+    </>
+  )
+}
+
+export default App
